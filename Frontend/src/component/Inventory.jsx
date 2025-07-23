5e66235f521b55140b070dfe093424e22485ce48
Saurabh kumar <saurabhkumar20843@gmail.com>
2025-07-23 19:31:48 +0530

    start

--- Frontend/src/component/Inventory.jsx
+++ Frontend/src/component/Inventory.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import "./Inventory.css"
 const Inventory = () => {
-  return (
-    <div>Inventory</div>
-  )
+    return (
+        <div>
+            <div className='main'>
+                <div className='upnav'>
+                You Currently have no inverntory. Add products to start managing your stock.
+                </div>
+                <div className='box'>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Inventory
