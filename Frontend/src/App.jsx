6258136665dc2350585156c2e231288e7dd211df
Aditya Kumar <adityasng420.ak@gmail.com>
2025-07-29 18:39:28 +0530

    updated app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,27 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
- import Inventory from './component/Inventory.jsx'
- import AddProduct from './component/AddProduct'
- import Dashboard from './component/Dashboard'
+import Inventory from './component/Inventory.jsx'
+import AddProduct from './component/AddProduct'
+import Dashboard from './component/Dashboard'
 import AboutProduct from './component/AboutProduct'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import Category from './component/Category.jsx';
 import CategoryDashbord from './component/CategoryDashbord.jsx'
- import Stocks from './component/Stocks.jsx'
- import StockDashbord from "./component/StockDashbord.jsx";
+import Stocks from './component/Stocks.jsx'
+import StockDashbord from "./component/StockDashbord.jsx";
 import PurchaseOrder from './component/Purchase/PurchaseOrder.jsx'
-=======
 import AddCategory from './component/AddCategory'
->>>>>>> Stashed changes
-=======
-import AddCategory from './component/AddCategory'
->>>>>>> Stashed changes
-=======
-import AddCategory from './component/AddCategory'
->>>>>>> Stashed changes
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,24 +22,12 @@ function App() {
       {/* <AddProduct /> */}
       {/* <Dashboard /> */}
       {/* <AboutProduct/> */}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-       {/* <Category/> */}
-       {/* <CategoryDashbord/> */}
-       {/* <Stocks/> */}
-       {/* <StockDashbord/> */}
-       <PurchaseOrder/>
-
-=======
-      <AddCategory/>
->>>>>>> Stashed changes
-=======
-      <AddCategory/>
->>>>>>> Stashed changes
-=======
-      <AddCategory/>
->>>>>>> Stashed changes
+      {/* <Category/> */}
+      {/* <CategoryDashbord/> */}
+      {/* <Stocks/> */}
+      {/* <StockDashbord/> */}
+      {/* <PurchaseOrder/> */}
+      {/* <AddCategory/> */}
     </>
   )
 }
