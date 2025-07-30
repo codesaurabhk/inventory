b9e0fa67f7fb64181e71dcb9d63810b3cd2c697f
Avinash-netario <135868337+codesaurabhk@users.noreply.github.com>
2025-07-30 18:28:34 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       {/* <ExpiryItems/> */}
       {/* <ExpiryItemsDashbord/> */}
       {/* <Damages/> */}
-      <Damagesdashbord/>
+      {/* <Damagesdashbord/> */}
     </>
   )
 }
