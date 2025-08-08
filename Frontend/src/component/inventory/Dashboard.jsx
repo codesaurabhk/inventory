ddd5764f972453084d9503dc1a3690828c68b95b
Saurabh kumar <saurabhkumar20843@gmail.com>
2025-08-08 17:37:14 +0530

    updated

--- Frontend/src/component/inventory/Dashboard.jsx
+++ Frontend/src/component/inventory/Dashboard.jsx
@@ -127,160 +127,155 @@ const Dashboard = () => {
                                 <Link to="/ChooseToAddProduct" className="btn btn-primary">Add Product</Link>
                             </div>
                         </div>
-                        <div className="toolbar-actions">
-                            <button className="btn btn-light" style={{ marginTop: '-10px' }}>Export</button>
-                            <Link to="/ChooseToAddProduct" className="btn btn-primary">Add Product</Link>
-                        </div>
-                    </div>
 
-                    <div className='toolbar-actions-th'>
-                        <div className="toolbar-titles">
-                            {searchdrop ? (
-                                <>
-                                    <div style={{ border: 'none', marginLeft: '20px', alignItems: 'center', display: 'flex' }}>
-                                        <IoIosSearch style={{ fontSize: '25px' }} />
-                                        <input type='text' placeholder='Search Here' style={{ border: 'none', outline: 'none', fontSize: '20px' }} />
-                                    </div>
-                                </>
-                            ) : (
-                                <>
-                                    <button className="toolbar-filter-btn">All</button>
-                                </>
-                            )}
-                        </div>
-
-                        <div className="toolbar-action" style={{ marginTop: "4px" }}>
-                            {searchdrop ? (
-                                <></>) : (<>
-                                    <button className="icon-btn " value={searchdrop} onClick={handleSearchDropChange}><IoSearch /> <CgSortAz style={{ fontSize: '30px' }} /></button>
-                                </>)}
-                            <button className="icon-btn" onClick={handleClear}><TbArrowsSort /></button>
-                        </div>
-                    </div>
-
-                    {searchdrop ? (
-                        <>
-                            <div className='' style={{ display: 'flex', justifyContent: 'space-between', padding: '5px', borderBottom: '2px solid #E6E6E6' }}>
-
-                                <div className="toolbar-titles" style={{ marginTop: "4px", display: 'flex', gap: '10px' }}>
-                                    <div style={{ border: '2px solid #ccc', padding: '1px 5px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}>
-                                        <button className="icon-btn" style={{ outline: 'none', border: 'none', color: '#555252' }}> Filter <CgSortAz style={{ fontSize: '30px' }} /></button>
-                                    </div>
-
-                                    <div
-                                        style={{ border: categoryValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
-                                        value={categoryValue}
-                                        onChange={handleCategoryChange}>
-                                        <select className="icon-btn" style={{ outline: 'none', border: 'none', color: categoryValue ? '#1368EC' : '#555252' }}>
-                                            <option value="" style={{ color: '#555252' }}>Category</option>
-                                            <option value="c1" style={{ color: '#555252' }}>Category 1</option>
-                                            <option value="c2" style={{ color: '#555252' }}>Category 2</option>
-                                        </select>
-                                    </div>
-
-                                    <div
-                                        style={{ border: socketValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
-                                        value={socketValue}
-                                        onChange={handleSocketChange}>
-                                        <select className="icon-btn" style={{ outline: 'none', border: 'none', color: socketValue ? '#1368EC' : '#555252' }}>
-                                            <option value="" style={{ color: '#555252' }}>Socket Level</option>
-                                            <option value="sl1" style={{ color: '#555252' }}>Last 7 days</option>
-                                        </select>
-                                    </div>
-
-                                    <div
-                                        style={{ border: warehouseValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
-                                        value={warehouseValue}
-                                        onChange={handleWarehouseChange}>
-                                        <select className="icon-btn" style={{ outline: 'none', border: 'none', color: warehouseValue ? '#1368EC' : '#555252' }}>
-                                            <option value="" style={{ color: '#555252' }}>Warehouse</option>
-                                            <option value="wh1" style={{ color: '#555252' }}>Warehouse 1</option>
-                                        </select>
-                                    </div>
-
-                                    <div
-                                        style={{ border: exprationValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
-                                        value={exprationValue}
-                                        onChange={handleExprationChange}>
-                                        <select className="icon-btn" style={{ outline: 'none', border: 'none', color: exprationValue ? '#1368EC' : '#555252' }}>
-                                            <option value="" style={{ color: '#555252' }}>Expiration</option>
-                                            <option value="e1" style={{ color: '#555252' }}>Expiration 1</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div className="toolbar-action" style={{ marginTop: "4px" }}>
-                                    <button className="icon-btn ">Clear</button>
-                                </div>
-
+                        <div className='toolbar-actions-th'>
+                            <div className="toolbar-titles">
+                                {searchdrop ? (
+                                    <>
+                                        <div style={{ border: 'none', marginLeft: '20px', alignItems: 'center', display: 'flex' }}>
+                                            <IoIosSearch style={{ fontSize: '25px' }} />
+                                            <input type='text' placeholder='Search Here' style={{ border: 'none', outline: 'none', fontSize: '20px' }} />
+                                        </div>
+                                    </>
+                                ) : (
+                                    <>
+                                        <button className="toolbar-filter-btn">All</button>
+                                    </>
+                                )}
                             </div>
-                        </>
-                    ) : (<></>)}
 
-                </div>
-
-                {/* Product Table */}
-                <div>
-                    <table className="product-table">
-                        <thead>
-                            <tr style={{ color: "#676767" }}>
-                                <th><input type="checkbox" /></th>
-                                <th>Product</th>
-                                <th>Status</th>
-                                <th>Inventory</th>
-                                <th>Category</th>
-                                <th>Stored In</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {paginatedData.map((product, index) => (
-                                <tr key={index}>
-                                    <td><input type="checkbox" /></td>
-                                    <td>
-                                        <Link to="/AboutProduct" style={{ textDecoration: 'none', color: 'black' }} >
-                                            <img
-                                                src={product.image}
-                                                alt={product.name}
-                                                className="product-img"
-                                            />
-                                            {product.name}
-                                        </Link>
-                                    </td>
-                                    <td> <span className={`status ${product.status.toLowerCase()}`}>{product.status}</span></td>
-                                    <td>{product.inventory}</td>
-                                    <td>{product.category}</td>
-                                    <td>{product.stored}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-
-                    <div className="pagination">
-                        <div className="pagination-boxx">{itemsPerPage} per page</div>
-                        <div className="pagination-boxx pagination-info">
-                            <span>{startIndex + 1}-{Math.min(endIndex, totalItems)} of {totalItems}</span>
-                            <span style={{ color: "grey" }}> | </span>
-                            <button
-                                disabled={currentPage === 1}
-                                onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-                                className="pagination-arrow"
-                            >
-                                <FaAngleLeft />
-                            </button>
-                            <button
-                                disabled={currentPage === totalPages}
-                                onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
-                                className="pagination-arrow"
-                            >
-                                <FaChevronRight />
-                            </button>
+                            <div className="toolbar-action" style={{ marginTop: "4px" }}>
+                                {searchdrop ? (
+                                    <></>) : (<>
+                                        <button className="icon-btn " value={searchdrop} onClick={handleSearchDropChange}><IoSearch /> <CgSortAz style={{ fontSize: '30px' }} /></button>
+                                    </>)}
+                                <button className="icon-btn" onClick={handleClear}><TbArrowsSort /></button>
+                            </div>
                         </div>
+
+                        {searchdrop ? (
+                            <>
+                                <div className='' style={{ display: 'flex', justifyContent: 'space-between', padding: '5px', borderBottom: '2px solid #E6E6E6' }}>
+
+                                    <div className="toolbar-titles" style={{ marginTop: "4px", display: 'flex', gap: '10px' }}>
+                                        <div style={{ border: '2px solid #ccc', padding: '1px 5px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}>
+                                            <button className="icon-btn" style={{ outline: 'none', border: 'none', color: '#555252' }}> Filter <CgSortAz style={{ fontSize: '30px' }} /></button>
+                                        </div>
+
+                                        <div
+                                            style={{ border: categoryValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
+                                            value={categoryValue}
+                                            onChange={handleCategoryChange}>
+                                            <select className="icon-btn" style={{ outline: 'none', border: 'none', color: categoryValue ? '#1368EC' : '#555252' }}>
+                                                <option value="" style={{ color: '#555252' }}>Category</option>
+                                                <option value="c1" style={{ color: '#555252' }}>Category 1</option>
+                                                <option value="c2" style={{ color: '#555252' }}>Category 2</option>
+                                            </select>
+                                        </div>
+
+                                        <div
+                                            style={{ border: socketValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
+                                            value={socketValue}
+                                            onChange={handleSocketChange}>
+                                            <select className="icon-btn" style={{ outline: 'none', border: 'none', color: socketValue ? '#1368EC' : '#555252' }}>
+                                                <option value="" style={{ color: '#555252' }}>Socket Level</option>
+                                                <option value="sl1" style={{ color: '#555252' }}>Last 7 days</option>
+                                            </select>
+                                        </div>
+
+                                        <div
+                                            style={{ border: warehouseValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
+                                            value={warehouseValue}
+                                            onChange={handleWarehouseChange}>
+                                            <select className="icon-btn" style={{ outline: 'none', border: 'none', color: warehouseValue ? '#1368EC' : '#555252' }}>
+                                                <option value="" style={{ color: '#555252' }}>Warehouse</option>
+                                                <option value="wh1" style={{ color: '#555252' }}>Warehouse 1</option>
+                                            </select>
+                                        </div>
+
+                                        <div
+                                            style={{ border: exprationValue ? '2px dashed #1368EC' : '2px dashed #ccc', padding: '0px 10px 0px 3px', alignItems: 'center', display: 'flex', borderRadius: '6px' }}
+                                            value={exprationValue}
+                                            onChange={handleExprationChange}>
+                                            <select className="icon-btn" style={{ outline: 'none', border: 'none', color: exprationValue ? '#1368EC' : '#555252' }}>
+                                                <option value="" style={{ color: '#555252' }}>Expiration</option>
+                                                <option value="e1" style={{ color: '#555252' }}>Expiration 1</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div className="toolbar-action" style={{ marginTop: "4px" }}>
+                                        <button className="icon-btn ">Clear</button>
+                                    </div>
+
+                                </div>
+                            </>
+                        ) : (<></>)}
+
                     </div>
 
+                    {/* Product Table */}
+                    <div>
+                        <table className="product-table">
+                            <thead>
+                                <tr style={{ color: "#676767" }}>
+                                    <th><input type="checkbox" /></th>
+                                    <th>Product</th>
+                                    <th>Status</th>
+                                    <th>Inventory</th>
+                                    <th>Category</th>
+                                    <th>Stored In</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {paginatedData.map((product, index) => (
+                                    <tr key={index}>
+                                        <td><input type="checkbox" /></td>
+                                        <td>
+                                            <Link to="/AboutProduct" style={{ textDecoration: 'none', color: 'black' }} >
+                                                <img
+                                                    src={product.image}
+                                                    alt={product.name}
+                                                    className="product-img"
+                                                />
+                                                {product.name}
+                                            </Link>
+                                        </td>
+                                        <td> <span className={`status ${product.status.toLowerCase()}`}>{product.status}</span></td>
+                                        <td>{product.inventory}</td>
+                                        <td>{product.category}</td>
+                                        <td>{product.stored}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
 
+                        <div className="pagination">
+                            <div className="pagination-boxx">{itemsPerPage} per page</div>
+                            <div className="pagination-boxx pagination-info">
+                                <span>{startIndex + 1}-{Math.min(endIndex, totalItems)} of {totalItems}</span>
+                                <span style={{ color: "grey" }}> | </span>
+                                <button
+                                    disabled={currentPage === 1}
+                                    onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
+                                    className="pagination-arrow"
+                                >
+                                    <FaAngleLeft />
+                                </button>
+                                <button
+                                    disabled={currentPage === totalPages}
+                                    onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+                                    className="pagination-arrow"
+                                >
+                                    <FaChevronRight />
+                                </button>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
-            </div>
-        </div >
+            </div >
         </>
     );
 };
