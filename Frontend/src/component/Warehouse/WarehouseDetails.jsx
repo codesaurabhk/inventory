6c379a1f7cdd858caf6dbcfb4b6fccd23e1c2559
itsJitu <diwakarkumar9451@gmail.com>
2025-08-11 12:50:44 +0530

    updated donut data & line chart data

--- Frontend/src/component/Warehouse/WarehouseDetails.jsx
+++ Frontend/src/component/Warehouse/WarehouseDetails.jsx
@@ -3,8 +3,62 @@ import { MdArrowForwardIos } from "react-icons/md";
 import { TbMoneybag } from "react-icons/tb";
 import RadioActive from "../images/Radioactive.png";
 import CircleLogo from "../images/Circlelogo.png";
+import DonutChart from "react-donut-chart";
+import LineChart from "react-linechart";
 
 function WarehouseDetails() {
+  const chartData = [
+    {
+      label: "Cement",
+      value: 30,
+    },
+    {
+      label: "pipe",
+      value: 25,
+    },
+    {
+      label: "Iron Rod",
+      value: 21,
+    },
+    {
+      label: "Sand",
+      value: 5,
+    },
+    {
+      label: "Other",
+      value: 3,
+    },
+  ];
+
+  const lineChartData = [
+    {
+      color: "#1368ec",
+      name: "Sold",
+      points: [
+        { x: 1, y: 40 },
+        { x: 2, y: 50 },
+        { x: 3, y: 35 },
+        { x: 4, y: 55 },
+        { x: 5, y: 70 },
+        { x: 6, y: 90 },
+      ],
+    },
+    {
+      color: "#BBE1FF",
+      name: "Purchase",
+      points: [
+        { x: 1, y: 45 },
+        { x: 2, y: 60 },
+        { x: 3, y: 40 },
+        { x: 4, y: 50 },
+        { x: 5, y: 65 },
+        { x: 6, y: 75 },
+      ],
+    },
+  ];
+
+  const monthLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
+
   return (
     <div>
       {/* Header */}
@@ -102,7 +156,9 @@ function WarehouseDetails() {
             <img src={CircleLogo} alt="money" />
           </div>
           <div className="bag-content">
-            <span style={{ color: "#676767", marginTop: "50px" }}>low Stock</span>
+            <span style={{ color: "#676767", marginTop: "50px" }}>
+              low Stock
+            </span>
             <br />
             <span style={{ textAlign: "left" }}>
               <b>₹12</b>
@@ -145,37 +201,194 @@ function WarehouseDetails() {
 
       {/* basic detials of warehous */}
 
-      <div style={{marginTop: '15px', backgroundColor:'#fff', borderRadius:'8px', padding:'10px 16px'}}>
-        <div style={{gap: '10px', marginBottom:'20px'}}>
-            <span>Warehouse Name</span>
+      <div
+        style={{
+          marginTop: "15px",
+          backgroundColor: "#fff",
+          borderRadius: "8px",
+          padding: "10px 16px",
+        }}
+      >
+        <div style={{ gap: "10px", marginBottom: "20px" }}>
+          <span>Warehouse Name</span>
+          <br />
+          <span>Wh-001</span>
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <span
+              style={{ color: "#262626", fontWeight: "400", fontSize: "16px" }}
+            >
+              Owner
+            </span>
             <br />
-            <span>Wh-001</span>
+            <span
+              style={{ color: "#676767", fontWeight: "400", fontSize: "16px" }}
+            >
+              Ajay Kumar
+            </span>
+          </div>
+
+          <div>
+            <span
+              style={{ color: "#262626", fontWeight: "400", fontSize: "16px" }}
+            >
+              Branch
+            </span>
+            <br />
+            <span
+              style={{ color: "#676767", fontWeight: "400", fontSize: "16px" }}
+            >
+              Pune
+            </span>
+          </div>
+
+          <div>
+            <span
+              style={{ color: "#262626", fontWeight: "400", fontSize: "16px" }}
+            >
+              Contact No.{" "}
+            </span>
+            <br />
+            <span
+              style={{ color: "#676767", fontWeight: "400", fontSize: "16px" }}
+            >
+              Ajay Kumar
+            </span>
+          </div>
+
+          <div>
+            <span
+              style={{ color: "#262626", fontWeight: "400", fontSize: "16px" }}
+            >
+              Total Available Item
+            </span>
+            <br />
+            <span
+              style={{ color: "#676767", fontWeight: "400", fontSize: "16px" }}
+            >
+              86,477{" "}
+            </span>
+          </div>
         </div>
-        <div style={{display:'flex', justifyContent:'space-between'}}>
-            <div>
-                <span>Owner</span>
-                <br />
-                <span>Ajay Kumar</span>
-            </div>
+      </div>
 
-             <div>
-                <span>Owner</span>
-                <br />
-                <span>Ajay Kumar</span>
-            </div>
+      {/* Dougnut chart & chart js */}
+      <div style={{ display: "flex", gap: "24px" }}>
+        <div
+          style={{
+            backgroundColor: "#ffffff",
+            borderRadius: "8px",
+            padding: "16px",
+            gap: "16px",
+            marginTop: "20px",
+            width: "291.76px",
+            height: "519.76px",
+          }}
+        >
+          <span
+            style={{ color: "#262626", fontWeight: "500", fontSize: "16px" }}
+          >
+            Storage
+          </span>
+          <br />
 
-             <div>
-                <span>Owner</span>
-                <br />
-                <span>Ajay Kumar</span>
-            </div>
+          <div>
+            <span
+              style={{ color: "#262626", fontWeight: "400", fontSize: "16px" }}
+            >
+              Stroage Capacity
+            </span>
+            <br />
+            <span
+              style={{ color: "#1368ec", fontWeight: "400", fontSize: "16px" }}
+            >
+              30% Left
+            </span>
+          </div>
 
-             <div>
-                <span>Owner</span>
-                <br />
-                <span>Ajay Kumar</span>
-            </div>
+          {/* Donut Chart */}
+
+          {/* Donut Chart */}
+          <DonutChart
+            data={chartData}
+            colors={["#B8D2F9", "#4286F0", "#A1C3F7", "#B8D2F9", "#D0E1FB"]}
+            width={220}
+            height={220}
+            legend={false} // hide built-in legend
+            formatValues={(val) => `${val}%`}
+            strokeColor="transparent" // remove border
+            strokeWidth={0}
+            style={{
+              margin: "20px auto 0", // center chart
+              display: "block",
+            }}
+          />
+
+          {/* Legend under chart */}
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "12px",
+              marginTop: "12px",
+            }}
+          >
+            {chartData.map((item, index) => (
+              <div
+                key={index}
+                style={{ display: "flex", alignItems: "center", gap: "6px" }}
+              >
+                <div
+                  style={{
+                    width: "12px",
+                    height: "12px",
+                    backgroundColor: [
+                      "#B8D2F9",
+                      "#4286F0",
+                      "#A1C3F7",
+                      "#B8D2F9",
+                      "#D0E1FB",
+                    ][index],
+                    borderRadius: "2px",
+                  }}
+                />
+                <span style={{ fontSize: "14px", color: "#262626" }}>
+                  {item.label} ({item.value}%)
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
+
+        {/* Line Chart */}
+
+         <div
+          style={{
+            width: "auto",
+            height: "520px",
+            borderRadius: "8px",
+            padding: "24px",
+            gap: "8px",
+            backgroundColor: "#fff",
+            marginTop: "20px",
+          }}
+        >
+          <h3 style={{ marginBottom: "20px", color: "#262626" }}>
+            Sales & Purchases (6 Months)
+          </h3>
+          <LineChart
+            width={600}
+            height={400}
+            data={lineChartData}
+            xLabel="Months"
+            yLabel="Quantity"
+            hidePoints={false}
+            interpolate="linear"
+            xLabels={monthLabels}
+          />
+        </div> 
       </div>
     </div>
   );
