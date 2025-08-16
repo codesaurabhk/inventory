c42670354d9324278ef769bd780d8760fd01f5db
Diwakar Kumar <diwakarkumar9451@gmail.com>
2025-08-16 17:57:03 +0530

    Fixed table css use inline css

--- Frontend/src/component/Warehouse/WarehouseDetails.jsx
+++ Frontend/src/component/Warehouse/WarehouseDetails.jsx
@@ -104,7 +104,7 @@ function WarehouseDetails() {
       product: "Plywood Sheet",
       time: "2025-08-11 09:45 AM",
       qty: 25,
-      movementType: "IN",
+      movementType: "Stock IN",
       sourceDest: "Supplier: ABC Plywood Co.",
       reference: "PO-2345",
     },
@@ -112,7 +112,7 @@ function WarehouseDetails() {
       product: "Cement Bag (50kg)",
       time: "2025-08-11 10:15 AM",
       qty: 100,
-      movementType: "OUT",
+      movementType: " Stock OUT",
       sourceDest: "Project Site A",
       reference: "Invoice-5678",
     },
@@ -120,7 +120,7 @@ function WarehouseDetails() {
       product: "Iron Rod (12mm)",
       time: "2025-08-10 04:20 PM",
       qty: 50,
-      movementType: "IN",
+      movementType: " Stock IN",
       sourceDest: "Steel Works Pvt Ltd",
       reference: "PO-2398",
     },
@@ -128,7 +128,7 @@ function WarehouseDetails() {
       product: "Water Heater 15L",
       time: "2025-08-09 02:30 PM",
       qty: 8,
-      movementType: "OUT",
+      movementType: " Stock OUT",
       sourceDest: "Retail Customer",
       reference: "Sales-INV-9912",
     },
@@ -136,7 +136,7 @@ function WarehouseDetails() {
       product: "Sunmica Sheet",
       time: "2025-08-08 11:00 AM",
       qty: 40,
-      movementType: "IN",
+      movementType: "Stock IN",
       sourceDest: "Decor Supplies Ltd",
       reference: "PO-2410",
     },
@@ -616,26 +616,41 @@ function WarehouseDetails() {
               padding: "8px",
             }}
           >
-            Processing{" "}
+            Processing
           </span>
         </div>
 
         {/* Table */}
         <div>
-          <table className="customer-table">
-            <thead>
-              <tr style={{ backgroundColor: "#e6e6e6" }}>
-                <th>
+          <table
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              backgroundColor: "#fff",
+              padding: "16px 24px",
+            }}
+          >
+            <thead style={{ backgroundColor: "#f1f1f1" }}>
+              <tr
+                style={{
+                  color: "#676767",
+                  fontFamily: "roboto",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                <th style={{ padding: "12px 24px" }}>
                   <input type="checkbox" />
                 </th>
-                <th>Product</th>
-                <th>SKU</th>
-                <th>MRP</th>
-                <th>Available QTY</th>
-                <th>Unit Sold</th>
-                <th>Revennue</th>
+                <th style={{ padding: "12px 24px" }}>Product</th>
+                <th style={{ padding: "12px 24px" }}>SKU</th>
+                <th style={{ padding: "12px 24px" }}>MRP</th>
+                <th style={{ padding: "12px 24px" }}>Available QTY</th>
+                <th style={{ padding: "12px 24px" }}>Unit Sold</th>
+                <th style={{ padding: "12px 24px" }}>Revenue</th>
               </tr>
             </thead>
+
             <tbody>
               {sellingProducts.map((wholeseller, i) => (
                 <tr
@@ -643,10 +658,20 @@ function WarehouseDetails() {
                   onClick={() => handleCustomerClick(wholeseller)}
                   style={{ cursor: "pointer" }}
                 >
-                  <td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
                     <input type="checkbox" />
                   </td>
-                  <td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
                     <div className="customer-info">
                       <img
                         src="https://via.placeholder.com/32"
@@ -656,11 +681,46 @@ function WarehouseDetails() {
                       {wholeseller.product}
                     </div>
                   </td>
-                  <td>{wholeseller.sku}</td>
-                  <td>{wholeseller.mrp}</td>
-                  <td>{wholeseller.available}</td>
-                  <td>{wholeseller.unit}</td>
-                  <td>{wholeseller.revenue}</td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {wholeseller.sku}
+                  </td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {wholeseller.mrp}
+                  </td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {wholeseller.available}
+                  </td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {wholeseller.unit}
+                  </td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {wholeseller.revenue}
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -759,38 +819,6 @@ function WarehouseDetails() {
           </div>
         </div>
 
-        {/* godown 2 */}
-        {/* <div
-          style={{
-            marginTop: "20px",
-            backgroundColor: "#red",
-            borderRadius: "8px",
-            boxShadow: "0px 0px 8px 3px #0000001A",
-            padding: "16px",
-          }}
-        > */}
-        {/* Header */}
-        {/* <div
-            style={{
-              padding: "16px",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <span>Godown</span>
-            <span
-              style={{
-                color: "#1368EC",
-                display: "flex",
-                alignItems: "center",
-                gap: "4px",
-                cursor: "pointer",
-              }}
-            >
-             
-            </span>
-          </div> */}
-
         {/* Content */}
         <div
           style={{
@@ -864,7 +892,7 @@ function WarehouseDetails() {
         <div
           style={{
             padding: "16px 24px",
-            borderBottom: "1px solid #e6e6e6", 
+            borderBottom: "1px solid #e6e6e6",
             fontWeight: "500",
             fontSize: "18px",
             color: "#262626",
@@ -904,71 +932,96 @@ function WarehouseDetails() {
           </div>
         </div>
 
-        <div style={{ padding: "8px 24px", gap: "18px", display:'flex', justifyContent:'space-between', alignItems:'center' }}>
+        <div
+          style={{
+            padding: "8px 24px",
+            gap: "18px",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <div>
             <span
-            style={{
-              font: "Robot",
-              fontWeight: "400",
-              fontSize: "16px",
-              color: "#262626",
-              padding: "8px",
-            }}
-          >
-            All
-          </span>
-          <span
-            style={{
-              font: "Robot",
-              fontWeight: "400",
-              fontSize: "16px",
-              color: "#262626",
-              padding: "8px",
-            }}
-          >
-            Stock In
-          </span>
-          <span
-            style={{
-              font: "Robot",
-              fontWeight: "400",
-              fontSize: "16px",
-              color: "#262626",
-              padding: "8px",
-            }}
-          >
-            Stock Out
-          </span>
-          <span
-            style={{
-              font: "Robot",
-              fontWeight: "400",
-              fontSize: "16px",
-              color: "#262626",
-              padding: "8px",
-            }}
-          >
-            Transfer
-          </span>
-          <span
-            style={{
-              font: "Robot",
-              fontWeight: "400",
-              fontSize: "16px",
-              color: "#262626",
-              padding: "8px",
-            }}
-          >
-            Processing
-          </span>
+              style={{
+                font: "Robot",
+                fontWeight: "400",
+                fontSize: "16px",
+                color: "#262626",
+                padding: "8px",
+              }}
+            >
+              All
+            </span>
+            <span
+              style={{
+                font: "Robot",
+                fontWeight: "400",
+                fontSize: "16px",
+                color: "#262626",
+                padding: "8px",
+              }}
+            >
+              Stock In
+            </span>
+            <span
+              style={{
+                font: "Robot",
+                fontWeight: "400",
+                fontSize: "16px",
+                color: "#262626",
+                padding: "8px",
+              }}
+            >
+              Stock Out
+            </span>
+            <span
+              style={{
+                font: "Robot",
+                fontWeight: "400",
+                fontSize: "16px",
+                color: "#262626",
+                padding: "8px",
+              }}
+            >
+              Transfer
+            </span>
+            <span
+              style={{
+                font: "Robot",
+                fontWeight: "400",
+                fontSize: "16px",
+                color: "#262626",
+                padding: "8px",
+              }}
+            >
+              Processing
+            </span>
           </div>
+
           {/* three icon */}
-          <div style={{display:'flex', gap:'16px', alignItems:'center'}}>
-            <div style={{display:'flex',border:'1px solid #f1f1f1', padding:'6px', borderRadius:'4px', gap:'4px', color:'#676767' }}>
-              <CiSearch style={{fontSize:'20px'}} />
-              <IoFilter style={{fontSize:'20px'}} />
+          <div style={{ display: "flex", gap: "16px", alignItems: "center" }}>
+            <div
+              style={{
+                display: "flex",
+                border: "1px solid #f1f1f1",
+                padding: "6px",
+                borderRadius: "4px",
+                gap: "4px",
+                color: "#676767",
+              }}
+            >
+              <CiSearch style={{ fontSize: "20px" }} />
+              <IoFilter style={{ fontSize: "20px" }} />
             </div>
-            <div style={{color:'#676767', border:'1px solid #f1f1f1', padding:'4px', borderRadius:'4px' }}>
+            <div
+              style={{
+                color: "#676767",
+                border: "1px solid #f1f1f1",
+                padding: "4px",
+                borderRadius: "4px",
+              }}
+            >
               <LuArrowUpDown />
             </div>
           </div>
@@ -976,32 +1029,139 @@ function WarehouseDetails() {
 
         {/* Table */}
         <div>
-          <table className="customer-table">
-            <thead>
-              <tr style={{ backgroundColor: "#e6e6e6" }}>
-                <th>
+          <table
+            className="customer-table"
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              backgroundColor: "#fff",
+            }}
+          >
+            <thead style={{ backgroundColor: "#f1f1f1" }}>
+              <tr
+                style={{
+                  color: "#676767",
+                  fontFamily: "Roboto, sans-serif",
+                  fontSize: "16px",
+                  fontWeight: "400",
+                }}
+              >
+                <th style={{ padding: "12px 24px", textAlign: "left" }}>
                   <input type="checkbox" />
                 </th>
-                <th>Product</th>
-                <th>Time</th>
-                <th>QTY</th>
-                <th>Movement type</th>
-                <th>Source/Destination</th>
-                <th>Reference/Note</th>
+                <th style={{ padding: "12px 24px", textAlign: "left" }}>
+                  Product
+                </th>
+                <th style={{ padding: "12px 24px", textAlign: "left" }}>
+                  Time
+                </th>
+                <th style={{ padding: "12px 24px", textAlign: "left" }}>QTY</th>
+                <th style={{ padding: "12px 24px", textAlign: "left" }}>
+                  Movement type
+                </th>
+                <th style={{ padding: "12px 24px", textAlign: "left" }}>
+                  Source/Destination
+                </th>
+                <th style={{ padding: "12px 24px", textAlign: "left" }}>
+                  Reference/Note
+                </th>
               </tr>
             </thead>
+
             <tbody>
               {dummyData.map((item, index) => (
-                <tr key={index}>
-                  <td>
+                <tr key={index} style={{ cursor: "pointer" }}>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
                     <input type="checkbox" />
                   </td>
-                  <td>{item.product}</td>
-                  <td>{item.time}</td>
-                  <td>{item.qty}</td>
-                  <td><span className={`status ${item.movementType.toLowerCase()}`}>{item.movementType}</span></td>
-                  <td>{item.sourceDest}</td>
-                  <td>{item.reference}</td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {item.product}
+                  </td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {item.time}
+                  </td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {item.qty}
+                  </td>
+                  <td
+                    style={{ borderBottom: "1px solid #ddd", padding: "8px" }}
+                  >
+                    {(() => {
+                      const type = item.movementType.trim().toLowerCase(); // normalize
+
+                      if (type === "stock in") {
+                        return (
+                          <span
+                            style={{
+                              padding: "4px 12px",
+                              borderRadius: "20px",
+                              fontSize: "13px",
+                              fontWeight: "500",
+                              backgroundColor: "#DFFFE0", // green shade
+                            }}
+                          >
+                            {item.movementType}
+                          </span>
+                        );
+                      }
+
+                      if (type === "stock out") {
+                        return (
+                          <span
+                            style={{
+                              padding: "4px 12px",
+                              borderRadius: "20px",
+                              fontSize: "13px",
+                              fontWeight: "500",
+                              backgroundColor: "#FCE4E6", // red shade
+                            }}
+                          >
+                            {item.movementType}
+                          </span>
+                        );
+                      }
+
+                      // fallback (no color)
+                      return <span>{item.movementType}</span>;
+                    })()}
+                  </td>
+
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {item.sourceDest}
+                  </td>
+                  <td
+                    style={{
+                      padding: "12px 24px",
+                      borderBottom: "1px solid #e6e6e6",
+                    }}
+                  >
+                    {item.reference}
+                  </td>
                 </tr>
               ))}
             </tbody>
